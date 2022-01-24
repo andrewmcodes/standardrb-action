ab67e0a146430e6c007ca604703a9bc5f19b67f6
Jordan Elver <jordan@elver.me>
2022-01-24 16:15:55 +0000

    fixup! debugging

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -3,11 +3,13 @@
 set -e
 
 # this install standard for use in the next step
-gem install standard
+# gem install standard
 
 # does this run in the context of the project checkout? 
 # i think so
 cat Gemfile
+
+bundle install --with ci
 
 # this runs the standardrb binary and reports results to GitHub
 ruby /action/lib/index.rb
